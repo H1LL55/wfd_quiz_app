c086c319bfa17f6f3e6a7f62c59a8b564e5ad3e0
H1LL55 <jennahillss@gmail.com>
2025-03-04 14:34:11 +0000

    changed port

--- import_mysql.py
+++ import_mysql.py
@@ -191,5 +191,8 @@ def add_question():
 from waitress import serve
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render provides a PORT variable
+    app.run(host="0.0.0.0", port=port, debug=True)
